3d890d1269bd725a4d57e3cb637915f312f16079
Abdoulaye Aliou Sall <gallebaba71@gmail.com>
2024-09-15 00:49:00 +0000

    add two numbers

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        ListNode listNode = new ListNode();
         int[] nums1 = { 2,7,11,15};
         int target = 9;
         System.out.println("la Solution de "+ Arrays.toString(solution.twoSum(nums1, target)));
+        System.out.println("Add Two number = "+ solution.addTwoNumbers(new ListNode(123), new ListNode(345)).toString());
     }
 }
